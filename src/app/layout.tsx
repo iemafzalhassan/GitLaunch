26655ef55b0d069b148d909725f75c8ef6400045
Md. Afzal Hassan Ehsani <iemafzalhassan@gmail.com>
2025-08-17 05:35:33 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={`${spaceGrotesk.variable} ${sourceCodePro.variable} font-body antialiased`} suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className={`${spaceGrotesk.variable} ${sourceCodePro.variable} font-body antialiased`}>
         {children}
         <Toaster />
       </body>
