83e04d7ad80998c63550ba83e328b0db7bebb2ee
Md. Afzal Hassan Ehsani <iemafzalhassan@gmail.com>
2025-08-17 04:53:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`${spaceGrotesk.variable} ${sourceCodePro.variable} font-body antialiased`}>
+    <html lang="en" className="dark">
+      <body className={`${spaceGrotesk.variable} ${sourceCodePro.variable} font-body antialiased`} suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
